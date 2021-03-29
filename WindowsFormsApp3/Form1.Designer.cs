0f62b0f56531fdab3e4b28c898fd17eb0e4f71c5
Alexandre Scatolin Tem-Pass <74421450+scatolin12@users.noreply.github.com>
2021-03-29 09:10:54 -0300

    commit 1 - classserach

--- WindowsFormsApp3/Form1.Designer.cs
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WindowsFormsApp3
 {
-    partial class Form1
+    partial class ClassSearch
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,13 +28,201 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_carregarclasse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_classe = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_status = new System.Windows.Forms.Label();
+            this.Characteristic = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toignore = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.mainmaterial = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Result = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_help = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_carregarclasse
+            // 
+            this.btn_carregarclasse.Location = new System.Drawing.Point(12, 51);
+            this.btn_carregarclasse.Name = "btn_carregarclasse";
+            this.btn_carregarclasse.Size = new System.Drawing.Size(169, 23);
+            this.btn_carregarclasse.TabIndex = 2;
+            this.btn_carregarclasse.Text = "Load Class";
+            this.btn_carregarclasse.UseVisualStyleBackColor = true;
+            this.btn_carregarclasse.Click += new System.EventHandler(this.btn_carregarclasse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Class";
+            // 
+            // tb_classe
+            // 
+            this.tb_classe.Location = new System.Drawing.Point(12, 25);
+            this.tb_classe.Name = "tb_classe";
+            this.tb_classe.Size = new System.Drawing.Size(169, 20);
+            this.tb_classe.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Characteristic,
+            this.Value,
+            this.toignore});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(533, 499);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 591);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Status:";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.mainmaterial,
+            this.Result});
+            this.dataGridView2.Location = new System.Drawing.Point(607, 80);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(405, 499);
+            this.dataGridView2.TabIndex = 6;
+            this.dataGridView2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataGridView2_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(607, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_status
+            // 
+            this.lbl_status.AutoSize = true;
+            this.lbl_status.Location = new System.Drawing.Point(55, 591);
+            this.lbl_status.Name = "lbl_status";
+            this.lbl_status.Size = new System.Drawing.Size(0, 13);
+            this.lbl_status.TabIndex = 8;
+            // 
+            // Characteristic
+            // 
+            this.Characteristic.HeaderText = "Characteristic";
+            this.Characteristic.Name = "Characteristic";
+            this.Characteristic.ReadOnly = true;
+            this.Characteristic.Width = 280;
+            // 
+            // Value
+            // 
+            this.Value.HeaderText = "Value";
+            this.Value.Name = "Value";
+            this.Value.Width = 130;
+            // 
+            // toignore
+            // 
+            this.toignore.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.toignore.HeaderText = "To Ignore";
+            this.toignore.Name = "toignore";
+            this.toignore.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.toignore.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.toignore.Width = 78;
+            // 
+            // mainmaterial
+            // 
+            this.mainmaterial.HeaderText = "Material Model";
+            this.mainmaterial.MaxInputLength = 8;
+            this.mainmaterial.Name = "mainmaterial";
+            // 
+            // Result
+            // 
+            this.Result.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Result.DividerWidth = 8;
+            this.Result.HeaderText = "Result";
+            this.Result.Name = "Result";
+            // 
+            // btn_help
+            // 
+            this.btn_help.Location = new System.Drawing.Point(987, 9);
+            this.btn_help.Name = "btn_help";
+            this.btn_help.Size = new System.Drawing.Size(25, 26);
+            this.btn_help.TabIndex = 9;
+            this.btn_help.Text = "?";
+            this.btn_help.UseVisualStyleBackColor = true;
+            this.btn_help.Click += new System.EventHandler(this.btn_help_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(937, 51);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 23);
+            this.btn_clear.TabIndex = 10;
+            this.btn_clear.Text = "Clear All";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // ClassSearch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1024, 613);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_help);
+            this.Controls.Add(this.lbl_status);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.tb_classe);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_carregarclasse);
+            this.Name = "ClassSearch";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "Class Search";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_carregarclasse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_classe;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_status;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Characteristic;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn toignore;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mainmaterial;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Result;
+        private System.Windows.Forms.Button btn_help;
+        private System.Windows.Forms.Button btn_clear;
     }
 }
 
--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -7,14 +7,347 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Diagnostics;
+using sapfewse;
 
 namespace WindowsFormsApp3
 {
-    public partial class Form1 : Form
+    public partial class ClassSearch : Form
     {
-        public Form1()
+        public ClassSearch()
         {
             InitializeComponent();
+        }
+
+        private void btn_carregarclasse_Click(object sender, EventArgs e)
+        {
+
+            lbl_status.Text = "Loading Class...";
+
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+
+            StreamReader sr = new StreamReader(@"Q:\APPS\_NONSTD\WMO_SDP\DIMC\Classes.txt");
+
+            Boolean classeExiste = false;
+            string nome = tb_classe.Text.ToUpper();
+
+            while (!sr.EndOfStream)
+            {
+                var texto = sr.ReadLine();
+
+                if (texto.Contains(nome))
+                {
+                    classeExiste = true;
+                }
+
+            }
+
+            if (classeExiste == false || tb_classe.Text=="")
+            {
+                MessageBox.Show("Classe não encontrada");
+                return;
+            }
+
+            #region Check is Excel is installed in the PC on which program is executed
+
+            _Application excel = new _Excel.Application();
+            if (excel == null)
+            {
+
+                string errorMessage = "EXCEL could not be started." + "\n" +
+                    "This program is able to form reports only on PC with installed Excel. " + "\n" +
+                    "Check that your office installation is correct.";
+                MessageBox.Show(errorMessage);
+
+            }
+
+            #endregion
+
+            excel.Visible = false;
+
+            string fileName = "Busca DE-PARA.xlsm";
+            string pathToExcelXlsmFile = Path.Combine("Q:/APPS/_NONSTD/WMO_SDP/DIMC", fileName);
+
+            Workbook wb;
+            Worksheet ws;
+            wb = excel.Workbooks.Open(pathToExcelXlsmFile);
+            ws = wb.Sheets["Material Modelo"];
+
+            bool mesmaclasse = ws.Cells[1, 2].Value == tb_classe.Text.ToUpper();
+
+            
+
+                if (mesmaclasse)
+                    //Call VBA code
+                    excel.GetType().InvokeMember("Run", System.Reflection.BindingFlags.Default | System.Reflection.BindingFlags.InvokeMethod, null, excel, new Object[] { "atualizardados" });
+
+                else
+                {
+                    excel.GetType().InvokeMember("Run", System.Reflection.BindingFlags.Default | System.Reflection.BindingFlags.InvokeMethod, null, excel, new Object[] { "BaixarMateriaisClasse", tb_classe.Text });
+                }
+
+                ws.Cells[1, 2].Value = tb_classe.Text.ToUpper();
+            
+            if (ws.Cells[1,4].value == "ERRO AO CONECTAR SAP")
+            {
+                excel.DisplayAlerts = false;
+                //wb.Save();
+                wb.Close();
+                excel.Quit();
+                MessageBox.Show("ERROR: make sure you have an open SAP instance");
+                lbl_status.Text = "Open a SAP instance";
+                return;
+            }
+               
+            
+
+            ////Call VBA code
+            //excel.GetType().InvokeMember("Run", System.Reflection.BindingFlags.Default | System.Reflection.BindingFlags.InvokeMethod, null, excel, new Object[] { "BaixarMateriaisClasse", tb_classe.Text });
+
+
+            int Rcount = 0;
+
+            _Excel.Range Range = (_Excel.Range)ws.Columns[1];
+            Rcount = Range.CurrentRegion.Rows.Count - 3;
+
+            for (int i = 0; i < Rcount; i++)
+            {
+                dataGridView1.Rows.Add(ws.Cells[i + 4, 1].Value);
+            }
+
+            #region Close excel object - release memory from this application
+
+            excel.DisplayAlerts = false;
+            wb.Save();
+            wb.Close(true);
+            excel.Quit();
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(excel);
+
+            lbl_status.Text = "Done";
+
+            #endregion
+        }
+
+
+        /////////////////////////////////
+        ////////////////////////////////
+        /// BUSCAR
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            lbl_status.Text = "Searching for equivalent materials...";
+
+            _Application excel = new _Excel.Application();
+
+            int dtcount1 = dataGridView1.Rows.Count;
+            int dtcount2 = dataGridView2.Rows.Count;
+
+            if (dtcount1 <= 1)
+            {
+                MessageBox.Show("Classe dos materiais não carregada!");
+                return;
+            }
+
+            int cont = 0;
+            for (int i = 0; i < dtcount1; i++)
+            {
+
+                if (dataGridView1.Rows[i].Cells[1].Value != null || dataGridView1.Rows[i].Cells[2].Value != null)
+                    cont = cont + 1;
+            }
+
+            if (cont == 0)
+            {
+                MessageBox.Show("Não há valores diferenciando os materiais!");
+                return;
+            }
+
+            if (dtcount2 <= 1)
+            {
+                return;
+            }
+
+            excel.Visible = false;
+
+            string fileName = "Busca DE-PARA.xlsm";
+            string pathToExcelXlsmFile = Path.Combine("Q:/APPS/_NONSTD/WMO_SDP/DIMC", fileName);
+
+            Workbook wb; 
+            Worksheet ws;
+
+            wb = excel.Workbooks.Open(pathToExcelXlsmFile);
+
+            ws = wb.Sheets["Material Modelo"];
+
+            bool mesmaclasse = ws.Cells[1, 2].Value == tb_classe.Text.ToUpper();
+
+
+            if (mesmaclasse)
+                //Call VBA code
+                excel.GetType().InvokeMember("Run", System.Reflection.BindingFlags.Default | System.Reflection.BindingFlags.InvokeMethod, null, excel, new Object[] { "atualizardados" });
+
+            else
+            {
+                MessageBox.Show("Carregue novamente a classe");
+            }
+
+
+            for (int i = 0; i < dtcount2; i++)
+            {
+                dataGridView2.Rows[i].Cells[1].Value = null;
+            }
+
+
+            for (int i = 0; i < dtcount1; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[1].Value != null)
+                    ws.Cells[i + 4, 2].Value = dataGridView1.Rows[i].Cells[1].Value;
+                if (dataGridView1.Rows[i].Cells[2].Value != null)
+                {
+                    ws.Cells[i + 4, 3].Value = "X";
+                }
+            }
+           
+                for (int i = 0; i < dtcount2; i++)
+                {
+                    ws.Cells[i + 4, 5].Value = dataGridView2.Rows[i].Cells[0].Value;
+                }
+                //Call VBA code
+                excel.GetType().InvokeMember("Run", System.Reflection.BindingFlags.Default | System.Reflection.BindingFlags.InvokeMethod, null, excel, new Object[] { "Buscar" });
+
+         
+
+            if (ws.Cells[1, 4].value == "ERRO AO CONECTAR SAP")
+            {
+                excel.DisplayAlerts = false;
+                //wb.Save();
+                wb.Close();
+                excel.Quit();
+                MessageBox.Show("ERROR: make sure you have an open SAP instance");
+                return;
+            }
+
+            for (int i = 0; i < dtcount2; i++)
+            {
+                dataGridView2.Rows[i].Cells[1].Value = ws.Cells[i + 4, 6].Value;
+            }
+
+            excel.DisplayAlerts = false;
+            wb.Save();
+            wb.Close(true);
+
+
+            excel.Quit();
+            #region Close excel object - release memory from this application
+
+
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(excel);
+
+            lbl_status.Text = "Done";
+
+            #endregion
+        }
+
+
+
+        // copiar colar
+        private void dataGridView2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ((e.Shift && e.KeyCode == Keys.Insert) || (e.Control && e.KeyCode == Keys.V))
+
+            {
+
+                char[] rowSplitter = { '\r', '\n' };
+
+                char[] columnSplitter = { '\t' };
+
+                //get the text from clipboard
+
+                IDataObject dataInClipboard = Clipboard.GetDataObject();
+
+                string stringInClipboard = (string)dataInClipboard.GetData(DataFormats.Text);
+
+                //split it into lines
+
+                string[] rowsInClipboard = stringInClipboard.Split(rowSplitter, StringSplitOptions.RemoveEmptyEntries);
+
+                //get the row and column of selected cell in grid
+
+                int r = dataGridView2.SelectedCells[0].RowIndex;
+
+                int c = dataGridView2.SelectedCells[0].ColumnIndex;
+
+                //add rows into grid to fit clipboard lines
+
+                if (dataGridView2.Rows.Count < (r + rowsInClipboard.Length))
+
+                {
+
+                    dataGridView2.Rows.Add(r + rowsInClipboard.Length - dataGridView2.Rows.Count + 1);
+
+                }
+
+                //loop through the lines, split them into cells and place the values in the corresponding cell.
+
+                for (int iRow = 0; iRow < rowsInClipboard.Length; iRow++)
+
+                {
+
+                    //split row into cell values
+
+                    string[] valuesInRow = rowsInClipboard[iRow].Split(columnSplitter);
+
+                    //cycle through cell values
+
+                    for (int iCol = 0; iCol < valuesInRow.Length; iCol++)
+
+                    {
+
+                        //assign cell value, only if it within columns of the grid
+
+                        if (dataGridView2.ColumnCount - 1 >= c + iCol)
+
+                        {
+
+                            dataGridView2.Rows[r + iRow].Cells[c + iCol].Value = valuesInRow[iCol];
+
+                        }
+
+                    }
+
+                }
+                //label5.Text = (dataGridView2.Rows.Count - 1).ToString() + " " + "linhas";
+
+                //if (dataGridView2.Rows.Count < 40)
+                //{
+                //    label6.Text = "Tempo Aprox. = " + ((dataGridView2.Rows.Count - 1) * 1.5).ToString() + " " + "segundos";
+                //}
+                //else
+                //{
+                //    label6.Text = "Tempo Aprox. = " + ((dataGridView2.Rows.Count - 1) * 0.025).ToString() + " " + "minutos";
+                //}
+
+            }
+        }
+
+        private void btn_help_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("For help ou sugestions, contact scatolin@weg.net  \n\n Developed by: Alexandre Scatolin Tem-Pass \n Product Engineer - WMO");
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            tb_classe.Text = null;
+            lbl_status.Text = "";
         }
     }
 }
